fa78c440eea22effe7fd56d6ae4f8b12c487c172
zeppelin-43 <dantgud@gmail.com>
2020-03-15 15:27:27 +0000

    !

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-python3 crack.py -f users.md5 -r rainbow.md5 -a md5
-python3 crack.py -f users.sha1 -r rainbow.sha1 -a sha1
-python3 crack.py -f users.sha256 -r rainbow.sha256 -a sha256
+python3 cracker.py -f users.md5 -r md5.rainbow -o users_md5.out -b -m 5 -n 5
+python3 cracker.py -f users.sha1 -r sha1.rainbow -o users_sha1.out -b -m 5 -n 5
+python3 cracker.py -f users.sha256 -r sha256.rainbow -o users_sha256.out -b -m 5 -n 5 
 
-python3 crack.py -f users_salted.md5 -r rainbow.md5 -a md5
-python3 crack.py -f users_salted.sha1 -r rainbow.sha1 -a sha1
-python3 crack.py -f users_salted.sha256 -r rainbow.sha256 -a sha256
+python3 cracker.py -f users_salted.md5 -r md5_salted.rainbow -o users_salted_md5.out -b -m 4 -n 4
+python3 cracker.py -f users_salted.sha1 -r sha1_salted.rainbow -o users_salted_sha1.out -b -m 4 -n 4
+python3 cracker.py -f users_salted.sha256 -r sha256_salted.rainbow -o users_salted_sha256.out -b -m 4 -n 4
